f04112eb28c3bb86cf00cccf35b146e06cc66cfc
Chris Miles <c3349246@uon.edu.au>
2024-07-26 13:59:55 +1000

    add new components and skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import Filters from "./components/Filters"
+import Guess from "./components/Guess"
+import Results from "./components/Results"
+import Statistics from "./components/Statistics"
+import React from "react"
+
 function App() {
+
   return (
     <>
       <h1>New app</h1>
+      <Filters />
+      <Guess />
+      <Results />
+      <Statistics />
     </>
   )
 }
---
+++ src/components/Guess.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Guess() {
+    return (
+        <div>
+            <h3>Guess</h3>
+        </div>
+    )
+}
