e5ca396e9f57ccd8ba15b0c8b1fec1fa3675e8ce
Chris Miles <c3349246@uon.edu.au>
2024-07-27 15:27:44 +1000

    fix get countries and hardcode data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import Filters from "./components/Filters"
-import Guess from "./components/Guess"
-import Results from "./components/Results"
-import Statistics from "./components/Statistics"
-import React from "react"
+import Filters from "./components/Filters";
+import Guess from "./components/Guess";
+import Results from "./components/Results";
+import Statistics from "./components/Statistics";
+import React from "react";
+import CountryFetcher from "./services/CountryFetcher";
+import data from "./data";
 
 function App() {
+  const [filteredData, setFilteredData] = React.useState(data);
+
+  // React.useEffect(() => {
+  //   CountryFetcher.fetchCountries().then((returnedCountryData) => {
+  //     const returnedCountryNames = Object.values(returnedCountryData).map(
+  //       (country) => country.name.common
+  //     );
+  //     setCountries(returnedCountryNames);
+  //   });
+  // }, []);
 
   return (
     <>
@@ -13,8 +25,13 @@ function App() {
       <Guess />
       <Results />
       <Statistics />
+      <ul>
+        {filteredData.map((country) => (
+          <li>{country.name.common}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/services/CountryFetcher.js
+++ src/services/CountryFetcher.js
@@ -3,9 +3,7 @@ import axios from "axios";
 const url = "https://restcountries.com/v3.1/all";
 
 const fetchCountries = () => {
-  axios.get(url).then((response) => {
-    return response.data;
-  });
+  return axios.get(url).then((response) => response.data);
 };
 
 export default { fetchCountries };
