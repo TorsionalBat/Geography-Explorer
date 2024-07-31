37f1609f5eb1ba883f7da2d5fa70e983699fedbb
Chris Miles <c3349246@uon.edu.au>
2024-07-31 17:04:29 +1000

    update styling for resultset

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,26 +1,64 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import {
+  Typography,
+  Container,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Box,
+  Divider,
+} from "@mui/material";
 
 export default function Results({ filteredData, guessedCountries }) {
   return (
-    <div>
-      <Typography variant="h5">Results</Typography>
-      <div
-        className="scrollable-list"
-        style={{ maxHeight: 400, overflowY: "auto" }}
-      >
-        <ul>
-          {filteredData.map((country) =>
-            guessedCountries.includes(country.name.common.toLowerCase()) ? (
-              <li key={country.cca3} style={{ color: "green" }}>
-                {country.name.common}
-              </li>
-            ) : (
-              <li key={country.cca3}>{country.name.common}</li>
-            )
-          )}
-        </ul>
-      </div>
-    </div>
+    <Container>
+      <Box marginTop={2}>
+        <Typography variant="h4">Results</Typography>
+      </Box>
+      <Divider />
+      <Box marginTop={2}>
+        <div
+          className="scrollable-list"
+          style={{ maxHeight: 410, overflowY: "auto" }}
+        >
+          <List>
+            {filteredData.map((country, index) => (
+              <React.Fragment key={`${country.name}--${index}`}>
+                <ListItem>
+                  {/* <ListItemAvatar>
+                            
+                        </ListItemAvatar> */}
+                </ListItem>
+                <ListItemText
+                  primary={
+                    <Box
+                      position="relative"
+                      width="100%"
+                      display="flex"
+                      alignItems="center"
+                    >
+                      <div>Generic Text</div>
+                    </Box>
+                  }
+                />
+                <Divider />
+              </React.Fragment>
+            ))}
+          </List>
+          <ul>
+            {filteredData.map((country) =>
+              guessedCountries.includes(country.name.common.toLowerCase()) ? (
+                <li key={country.cca3} style={{ color: "green" }}>
+                  {country.name.common}
+                </li>
+              ) : (
+                <li key={country.cca3}>{country.name.common}</li>
+              )
+            )}
+          </ul>
+        </div>
+      </Box>
+    </Container>
   );
 }
