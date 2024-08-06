98b959640f051c135595b8ec662d139d5dae37cc
Chris Miles <c3349246@uon.edu.au>
2024-08-07 09:02:29 +1000

    add comments

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -10,22 +10,26 @@ export default function Statistics({ filteredData }) {
     numberOfContinents: 0,
   });
 
+  /*
+    Helper function that takes the rendered list of results/countries and determines relevant statistics to be displayed
+  */
   const countCountriesByDifficulty = (list) => {
     if (!list) {
-      return;
+      return; // If not defined
     }
+    // Thresholds that determine whether a country is regarded as easy, medium or hard
     const hardThreshold = 500000;
     const mediumThreshold = 15000000;
     let easyCount = 0;
     let mediumCount = 0;
     let hardCount = 0;
 
-    const continentsSet = new Set();
+    const continentsSet = new Set(); // Keep track of the different continents within the list - items can only occur once in a set
 
     list.forEach((country) => {
       const population = country.population;
       country.continents.forEach((continent) => {
-        continentsSet.add(continent);
+        continentsSet.add(continent); // add() only adds if there isn't an element with the same value in the set
       });
 
       if (population <= hardThreshold) {
@@ -42,9 +46,12 @@ export default function Statistics({ filteredData }) {
     return { easyCount, mediumCount, hardCount, numberOfContinents };
   };
 
+  /* 
+    Effect used to ensure that when the dependency array changes (new data is filtered) that the statistics are re-rendered
+  */
   React.useEffect(() => {
-    const newStats = countCountriesByDifficulty(filteredData);
-    setStatistics(newStats);
+    const newStats = countCountriesByDifficulty(filteredData); // Calculate statistics
+    setStatistics(newStats); // Update the statistics
   }, [filteredData]);
 
   return (
