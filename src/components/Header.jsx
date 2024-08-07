4adb96c220b946246e77fccbcb4bc6fa3cecbcbe
Chris Miles <c3349246@uon.edu.au>
2024-08-07 15:27:20 +1000

    add icon to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,16 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import { Avatar, IconButton } from "@mui/material";
 
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1, marginBottom: 2 }}>
       <AppBar position="static" sx={{ padding: 2, alignItems: "center" }}>
         <Toolbar>
+          <IconButton>
+            <Avatar src="./globe.svg" />
+          </IconButton>
           <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
             Country Explorer
           </Typography>
